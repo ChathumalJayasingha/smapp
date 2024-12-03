3d549f4084f962f90bfe86b861f6cc157afde590
Chathumal Jayasinghe <chathumal.j@arpicoinsurance.com>
2024-12-03 14:22:44 +0530

    feat - initial commit

---
+++ src/test/java/com/chathumal/MainTest.java
@@ -0,0 +1,16 @@
+package com.chathumal;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+class MainTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @AfterEach
+    void tearDown() {
+
+    }
+}
