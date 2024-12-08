41cb2840dbaf9c9c8b01edd570903ea9a5d0f33c
chathumal <chathumaljayasingha@hotmail.com>
2024-12-08 14:29:59 +0530

    perf - update Service layer

--- src/main/java/com/chathumal/smapp/service/custom/UserService.java
+++ src/main/java/com/chathumal/smapp/service/custom/UserService.java
@@ -1,5 +1,10 @@
 package com.chathumal.smapp.service.custom;
 
+import com.chathumal.smapp.entity.User;
+
 public interface UserService {
-    void addUser(String ID, String password)throws Exception;
+    void addUser(String name, String address, String contact, String email, String password) throws Exception;
+    User loginCheck(String email, String password) throws Exception;
+
+
 }
--- src/main/java/com/chathumal/smapp/service/custom/impl/UserServiceImpl.java
+++ src/main/java/com/chathumal/smapp/service/custom/impl/UserServiceImpl.java
@@ -9,16 +9,23 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 public class UserServiceImpl implements UserService {
-    UserDAO userDAO= (UserDAO) DAOFactory.getInstance().getDAO(DAOFactory.Type.USER);
+
+    UserDAO userDAO = (UserDAO) DAOFactory.getInstance().getDAO(DAOFactory.Type.USER);
 
     @Override
-    public void addUser(String ID, String password) throws Exception {
+    public void addUser(String name, String address, String contact, String email, String password) throws Exception {
         Session session = FactoryConfiguration.getInstance().getSession();
         userDAO.setSession(session);
         Transaction transaction = null;
         try {
             transaction = session.beginTransaction();
-            userDAO.add(new User(1,"a","HI","075685"));
+            User user = new User();
+            user.setName(name);
+            user.setAddress(address);
+            user.setContact(contact);
+            user.setEmail(email);
+            user.setPassword(password);
+            userDAO.add(user);
             transaction.commit();
         } catch (Throwable t) {
             transaction.rollback();
@@ -27,4 +34,27 @@ public class UserServiceImpl implements UserService {
             session.close();
         }
     }
+
+    @Override
+    public User loginCheck(String email, String password) throws Exception {
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = FactoryConfiguration.getInstance().getSession();
+            userDAO.setSession(session);
+            transaction = session.beginTransaction();
+            User user = userDAO.findByEmail(email);
+            return user;
+
+        } catch (Exception e) {
+            transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return null;
+
+    }
+
+
 }
