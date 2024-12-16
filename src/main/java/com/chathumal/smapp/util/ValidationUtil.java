c84a7c478eb5d35b797042d68e2a0d417a211d8c
Chathumal Jayasinghe <chathumal.j@arpicoinsurance.com>
2024-12-16 09:22:35 +0530

    add - user email and password validtion added after assert email passwords

---
+++ src/main/java/com/chathumal/smapp/util/ValidationUtil.java
@@ -0,0 +1,25 @@
+package com.chathumal.smapp.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ValidationUtil {
+    public static boolean isValidEmail(String email) {
+        if (email == null || email.trim().isEmpty()) {
+            return false;
+        }
+
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+        Pattern pattern = Pattern.compile(emailRegex);
+        Matcher matcher = pattern.matcher(email.trim());
+
+        return matcher.matches();
+    }
+
+    public static boolean isValidPassword(String password) {
+        if (password == null || password.trim().isEmpty()) {
+            return false;
+        }
+        return password.trim().length() >= 4;
+    }
+}
