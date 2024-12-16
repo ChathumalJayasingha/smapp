5cff014182897fa878edcdbf461b858f91a5986b
Chathumal Jayasinghe <chathumal.j@arpicoinsurance.com>
2024-12-16 15:30:48 +0530

    pref - update user DAO details

--- src/main/java/com/chathumal/smapp/dao/custom/impl/UserDAOImpl.java
+++ src/main/java/com/chathumal/smapp/dao/custom/impl/UserDAOImpl.java
@@ -3,28 +3,37 @@ package com.chathumal.smapp.dao.custom.impl;
 import com.chathumal.smapp.dao.CrudDAOImpl;
 import com.chathumal.smapp.dao.custom.UserDAO;
 import com.chathumal.smapp.entity.User;
+import com.chathumal.smapp.exception.NotFoundException;
 import org.hibernate.query.NativeQuery;
 
 import java.util.List;
 
-public class UserDAOImpl extends CrudDAOImpl<User,String> implements UserDAO {
+public class UserDAOImpl extends CrudDAOImpl<User, String> implements UserDAO {
 
 
     @Override
-    public User findByEmail(String email) {
-        NativeQuery nativeQuery = session.createNativeQuery("select * from user where email='"+email+"'");
-        List<Object[]> rows = nativeQuery.list();
+    public User findByEmail(String email) throws NotFoundException {
         User user = new User();
-        for(Object[] row : rows){
-            user.setId(Integer.parseInt(row[1].toString()));
-            user.setFulacs(Boolean.parseBoolean(row[0].toString()));
-            user.setAddress(row[2].toString());
-            user.setContact(row[3].toString());
-            user.setEmail(row[4].toString());
-            user.setName(row[5].toString());
-            user.setPassword(row[6].toString());
+        NativeQuery nativeQuery = session.createNativeQuery("select * from user where email='" + email + "'");
+        List<Object[]> rows = nativeQuery.list();
+        if (rows.size() == 0) {
+            throw new NotFoundException("User cannot found");
+        } else {
+            for (Object[] row : rows) {
+                user.setId(Integer.parseInt(row[1].toString()));
+                String fulacs = row[0].toString();
+                if (fulacs.equalsIgnoreCase("1")) {
+                    user.setFulacs(true);
+                } else {
+                    user.setFulacs(false);
+                }
+                user.setAddress(row[2].toString());
+                user.setContact(row[3].toString());
+                user.setEmail(row[4].toString());
+                user.setName(row[5].toString());
+                user.setPassword(row[6].toString());
+            }
         }
-        System.out.println("user = " + user);
         return user;
     }
 }
