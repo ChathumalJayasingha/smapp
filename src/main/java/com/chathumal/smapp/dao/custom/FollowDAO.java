4c5495466d93fb368eb784cdbc3fcfd67df51bd4
Chathumal Jayasinghe <chathumal.j@arpicoinsurance.com>
2024-12-16 17:55:31 +0530

    perf - create follow DAO

---
+++ src/main/java/com/chathumal/smapp/dao/custom/FollowDAO.java
@@ -0,0 +1,8 @@
+package com.chathumal.smapp.dao.custom;
+
+import com.chathumal.smapp.dao.CrudDAO;
+import com.chathumal.smapp.entity.Follow;
+
+
+public interface FollowDAO extends CrudDAO<Follow,Integer> {
+}
---
+++ src/main/java/com/chathumal/smapp/dao/custom/impl/FollowDAOImpl.java
@@ -0,0 +1,9 @@
+package com.chathumal.smapp.dao.custom.impl;
+
+import com.chathumal.smapp.dao.CrudDAOImpl;
+import com.chathumal.smapp.dao.custom.FollowDAO;
+import com.chathumal.smapp.entity.Follow;
+
+public class FollowDAOImpl extends CrudDAOImpl<Follow, Integer> implements FollowDAO {
+
+}
