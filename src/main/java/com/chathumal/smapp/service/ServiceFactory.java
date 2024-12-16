ee9bbb7c8a6fef5609e93471d84126508b19de5c
Chathumal Jayasinghe <chathumal.j@arpicoinsurance.com>
2024-12-16 17:56:54 +0530

    perf - update Service Factory

--- src/main/java/com/chathumal/smapp/service/ServiceFactory.java
+++ src/main/java/com/chathumal/smapp/service/ServiceFactory.java
@@ -1,25 +1,33 @@
 package com.chathumal.smapp.service;
 
 import com.chathumal.smapp.service.custom.impl.ContentServiceImpl;
+import com.chathumal.smapp.service.custom.impl.FollowServiceImpl;
 import com.chathumal.smapp.service.custom.impl.UserServiceImpl;
 
 public class ServiceFactory {
 
     private static ServiceFactory ServiceFactory;
+
     private ServiceFactory() {
 
     }
+
     public static ServiceFactory getInstance() {
         return (ServiceFactory == null) ? ServiceFactory = new ServiceFactory() : ServiceFactory;
     }
 
-    public enum Type{USER,CONTENT}
+    public enum Type {USER, CONTENT, FOLLOW}
 
     public Object getService(Type type) {
         switch (type) {
-            case USER:return new UserServiceImpl();
-            case CONTENT:return new ContentServiceImpl();
-            default: return null;
+            case USER:
+                return new UserServiceImpl();
+            case CONTENT:
+                return new ContentServiceImpl();
+            case FOLLOW:
+                return new FollowServiceImpl();
+            default:
+                return null;
         }
     }
 }
