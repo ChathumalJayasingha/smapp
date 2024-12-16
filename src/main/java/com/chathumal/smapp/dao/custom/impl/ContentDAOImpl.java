e994a1b7fb2957725cf58198f2d7c8829e234da4
Chathumal Jayasinghe <chathumal.j@arpicoinsurance.com>
2024-12-16 15:25:30 +0530

    add - create Content DAO

---
+++ src/main/java/com/chathumal/smapp/dao/custom/impl/ContentDAOImpl.java
@@ -0,0 +1,10 @@
+package com.chathumal.smapp.dao.custom.impl;
+
+import com.chathumal.smapp.dao.CrudDAOImpl;
+import com.chathumal.smapp.dao.custom.ContentDAO;
+import com.chathumal.smapp.dao.custom.UserDAO;
+import com.chathumal.smapp.entity.Content;
+import com.chathumal.smapp.entity.User;
+
+public class ContentDAOImpl extends CrudDAOImpl<Content, Integer> implements ContentDAO {
+}
