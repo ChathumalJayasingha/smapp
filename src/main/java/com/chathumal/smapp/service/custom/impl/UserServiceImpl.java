feea9807c9d15bf2cea5714dfac480d3fc6359eb
Chathumal Jayasinghe <chathumal.j@arpicoinsurance.com>
2024-12-16 15:34:48 +0530

    add - added user update option

--- src/main/java/com/chathumal/smapp/service/custom/impl/UserServiceImpl.java
+++ src/main/java/com/chathumal/smapp/service/custom/impl/UserServiceImpl.java
@@ -4,16 +4,21 @@ import com.chathumal.smapp.configuration.FactoryConfiguration;
 import com.chathumal.smapp.dao.DAOFactory;
 import com.chathumal.smapp.dao.custom.UserDAO;
 import com.chathumal.smapp.entity.User;
+import com.chathumal.smapp.exception.DuplicateEntryException;
+import com.chathumal.smapp.exception.NotFoundException;
 import com.chathumal.smapp.service.custom.UserService;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.exception.ConstraintViolationException;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 public class UserServiceImpl implements UserService {
 
     UserDAO userDAO = (UserDAO) DAOFactory.getInstance().getDAO(DAOFactory.Type.USER);
 
     @Override
-    public void addUser(String name, String address, String contact, String email, String password) throws Exception {
+    public boolean addUser(String name, String address, String contact, String email, String password, boolean fulacs) throws DuplicateEntryException,Exception {
         Session session = FactoryConfiguration.getInstance().getSession();
         userDAO.setSession(session);
         Transaction transaction = null;
@@ -25,7 +30,39 @@ public class UserServiceImpl implements UserService {
             user.setContact(contact);
             user.setEmail(email);
             user.setPassword(password);
+            user.setFulacs(fulacs);
             userDAO.add(user);
+            transaction.commit();
+            return true;
+        } catch (ConstraintViolationException t) {
+            transaction.rollback();
+            t.printStackTrace();
+            throw new DuplicateEntryException("Please check email and mobile number");
+        } catch (Exception t) {
+            transaction.rollback();
+            t.printStackTrace();
+            throw new DuplicateEntryException(t.getMessage());
+        } finally {
+            session.close();
+        }
+    }
+
+    @Override
+    public void updateUser(Integer id,String name, String address, String contact, String email, String password, boolean fulacs) throws Exception {
+        Session session = FactoryConfiguration.getInstance().getSession();
+        userDAO.setSession(session);
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            User user = new User();
+            user.setId(id);
+            user.setName(name);
+            user.setAddress(address);
+            user.setContact(contact);
+            user.setEmail(email);
+            user.setPassword(password);
+            user.setFulacs(fulacs);
+            userDAO.update(user);
             transaction.commit();
         } catch (Throwable t) {
             transaction.rollback();
@@ -36,7 +73,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public User loginCheck(String email, String password) throws Exception {
+    public User findByEmail(String email) throws NotFoundException {
         Transaction transaction = null;
         Session session = null;
         try {
@@ -45,7 +82,6 @@ public class UserServiceImpl implements UserService {
             transaction = session.beginTransaction();
             User user = userDAO.findByEmail(email);
             return user;
-
         } catch (Exception e) {
             transaction.rollback();
             e.printStackTrace();
@@ -53,7 +89,6 @@ public class UserServiceImpl implements UserService {
             session.close();
         }
         return null;
-
     }
 
 
